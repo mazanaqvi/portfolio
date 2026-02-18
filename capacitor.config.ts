86c7f81ea04abd3993f1e5f09edde89b232433c9
Ali Hamza <ahmadhamzanaqvi@gmail.com>
2026-02-18 23:37:25 +0500

    capacitor native code added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.xrossapps.portfolio',
+  appName: 'Ali Portfolio',
+  webDir: 'dist'
+};
+
+export default config;
